d978d89ed5dbf6cab701d10d75accb6327430b50
emi <emilcastonguay@gmail.com>
2023-12-18 23:39:35 -0500

    starting to test with all the args

--- pipex.c
+++ pipex.c
@@ -5,22 +5,22 @@ int	main(int argc, char **argv)
 	int infile;
 	int outfile;
 
+	if (argc < 3)
 	infile = open(argv[1], O_RDONLY | O_CLOEXEC);
 	if (infile == -1)
 	{
 		close(infile);
 		exit(EXIT_FAILURE);
 	}
-	outfile = open(argv[argc - 1], O_CREAT | O_TRUNC | O_WRONLY |O_CLOEXEC, 0644);
+	outfile = open(argv[argc - 1], O_CREAT | O_TRUNC | O_WRONLY | O_CLOEXEC, 0644);
 	if (outfile == -1)
 	{
 		close(infile);
 		close(outfile);
 		exit(EXIT_FAILURE);
 	}
-
 	write(outfile, "Hello World", 11);
-
+	printf("Hello World\n%i", argc);
     close(infile);
     close(outfile);
 }
