508bd11dde7aa32de35b82d22667228ce90b88d7
E-Cast <emilcastonguay@gmail.com>
2024-01-12 22:17:51 -0500

    ready

--- pipex.c
+++ pipex.c
@@ -1,36 +1,19 @@
-#include <fcntl.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecastong <ecastong@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/12 22:17:26 by ecastong          #+#    #+#             */
+/*   Updated: 2024/01/12 22:17:28 by ecastong         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "pipex.h"
 
 int	main(int argc, char **argv)
 {
-	int infile;
-	int outfile;
 
-	if (argc < 3)
-		return 0;
-	infile = open(argv[1], O_RDONLY | O_CLOEXEC); 
-	if (infile == -1)
-	{
-		close(infile);
-		exit(EXIT_FAILURE);
-	}
-	outfile = open(argv[argc - 1], O_CREAT | O_TRUNC | O_WRONLY | O_CLOEXEC, 0644);
-	if (outfile == -1)
-	{
-		close(infile);
-		close(outfile);
-		exit(EXIT_FAILURE);
-	}
-	printf("Hello\n%i", argc);
-	char *const args[] = {"grep", "test", NULL};
-	char *const envp[] = {NULL};
-	dup2(infile, STDIN_FILENO);
-	close(infile);
-	dup2(outfile, STDOUT_FILENO);
-	close(outfile);
-	execve("/bin/grep", args, envp);
-	// write(outfile, "Hello World", 11);
-	printf("World\n");
-    close(infile);
-    close(outfile);
 }
