4b32f5014d5f1b5fce72267d1343bbe2bf920c3e
E-Cast <emilcastonguay@gmail.com>
2024-03-03 19:53:13 -0500

    string segmentation working, to implement in pipex

--- test.c
+++ test.c
@@ -1,16 +1,51 @@
 #include <stdio.h>
 #include "libft.h"
 
+// char	*ft_substr(char *str, int start, int len)
+// {
+// 	char	*sub_str;
+// 	char	*str_ptr;
+// 	size_t	i;
+
+// 	sub_str = (char *)ft_calloc(len + 1, sizeof(char));
+// 	if (!sub_str)
+// 		// terminate
+// 	str_ptr = (char *)str;
+// 	i = 0;
+// 	while (i < len)
+// 	{
+// 		sub_str[i] = str_ptr[start + i];
+// 		i++;
+// 	}
+// 	sub_str[i] = '\0';
+// 	return (sub_str);
+// }
+
 char	*make_segment(char *str, int start, int end)
 {
-	int	ix;
+	char	*segment;
+	int		str_inx;
+	int		seg_inx;
+	char	quote;
 
-	ix = start;
-	while (ix < end)
+	segment = ft_calloc((end - start) + 1, sizeof(char));
+	if (segment == NULL)
+		return (NULL); //terminate
+	str_inx = start;
+	seg_inx = 0;
+	quote = 0;
+	while (str_inx < end)
 	{
-		
-		ix++;
+		if (quote == 0 && (str[str_inx] == '\'' || str[str_inx] == '\"'))
+			quote = str[str_inx];
+		else if (quote != 0 && (str[str_inx] == '\'' || str[str_inx] == '\"'))
+			quote = 0;
+		else
+			segment[seg_inx++] = str[str_inx];
+		str_inx++;
 	}
+	my_strrealloc(&segment);
+	return (segment);
 }
 
 char	**segment_str(char **array, char *str)
@@ -26,8 +61,8 @@ char	**segment_str(char **array, char *str)
 	{
 		if (str[ix] == ' ' && quote == 0)
 		{
-			*array = ft_substr(str, start, ix - start);
-			*array++;
+			*array = make_segment(str, start, ix);
+			array++;
 			start = ix + 1;
 		}
 		else if (quote == 0 && (str[ix] == '\'' || str[ix] == '\"'))
@@ -37,7 +72,7 @@ char	**segment_str(char **array, char *str)
 		ix++;
 	}
 	if (start != ix)
-		*array = ft_substr(str, start, ix - start);
+		*array = make_segment(str, start, ix);
 	return (array);
 }
 
@@ -72,8 +107,11 @@ int	main(int argc, char **argv)
 
 	printf("%s\n%i\n", argv[argc - 1], count_segments(argv[argc - 1]));
 	array = ft_calloc(count_segments(argv[argc - 1]) + 1, sizeof(char *));
+	if (array )
 	segment_str(array, argv[argc - 1]);
 	index = 0;
 	while (array && array[index])
 		printf("%s\n", array[index++]);
+
+	// printf("%s\n" )
 }
